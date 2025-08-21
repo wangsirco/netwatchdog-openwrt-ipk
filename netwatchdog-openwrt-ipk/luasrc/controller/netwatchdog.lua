e877510ebabbfff60498226f925079516ba7b50e
iWSheng <101884559+wangsirco@users.noreply.github.com>
2025-08-22 04:18:36 +0700

    Delete netwatchdog-openwrt-ipk directory

--- netwatchdog-openwrt-ipk/luasrc/controller/netwatchdog.lua
+++
@@ -1,5 +0,0 @@
-module("luci.controller.netwatchdog", package.seeall)
-
-function index()
-    entry({"admin", "services", "netwatchdog"}, cbi("netwatchdog"), _("NetWatchdog"), 80)
-end
--- netwatchdog-openwrt-ipk/luasrc/model/cbi/netwatchdog.lua
+++
@@ -1,9 +0,0 @@
-local m = Map("netwatchdog", "PPPoE网络自动检测与修复")
-
-s = m:section(TypedSection, "main", "")
-s.addremove = false
-s.anonymous = true
-
-enable = s:option(Flag, "enabled", "启用NetWatchdog", "自动监控并修复网络异常")
-
-return m
